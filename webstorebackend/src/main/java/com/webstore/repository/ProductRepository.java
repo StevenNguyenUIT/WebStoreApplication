76fc2f683fec2be8d89664ad0ec46636adec255e
vantypham <phamvanty102023@gmail.com>
2023-11-17 15:26:52 -0600

    ty add order api

--- webstorebackend/src/main/java/com/webstore/repository/ProductRepository.java
+++ webstorebackend/src/main/java/com/webstore/repository/ProductRepository.java
@@ -9,7 +9,5 @@ import java.util.List;
 @Repository
 public interface ProductRepository extends MongoRepository<Product, String> {
     Product findByProductNumber(String productNumber);
-//    Product findByName(String productName);//find by exact name, return a product
     List<Product> findByNameContaining(String productName);// return a list of products contains name
-//    void removeByProductNumber(String productNumber);
 }
--- webstorebackend/src/main/java/com/webstore/service/OrderService.java
+++ webstorebackend/src/main/java/com/webstore/service/OrderService.java
@@ -1,4 +1,64 @@
 package com.webstore.service;
 
+import com.webstore.domain.Order;
+import com.webstore.repository.OrderRepository;
+import com.webstore.service.adapter.OrderAdapter;
+import com.webstore.service.dto.OrderDTO;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Service
 public class OrderService {
+
+    @Autowired
+    OrderRepository repo;
+
+    public OrderDTO createOrder(OrderDTO orderDTO) {
+        String generatedString = RandomStringUtils.randomAlphabetic(10);
+        orderDTO.setOrderId(generatedString);
+        orderDTO.setStatus("PLACED");
+        orderDTO.setDate(new Date());
+        repo.save(OrderAdapter.toObj(orderDTO));
+        return orderDTO;
+    }
+
+    public void updateOrderStatus(String orderId, String status) {
+        Order order = repo.findByOrderId(orderId);
+        if (order != null) {
+            order.setStatus(status);
+            repo.save(order);
+        }
+    }
+
+
+    public OrderDTO getOrderDetail(String orderId) {
+        return OrderAdapter.toDto(repo.findByOrderId(orderId));
+    }
+
+    public List<OrderDTO> getAllOrders() {
+        List<Order> list = repo.findAll();
+        List<OrderDTO> dtoList = new ArrayList<>();
+        if (list != null && !list.isEmpty()) {
+            for (Order o: list) {
+                dtoList.add(OrderAdapter.toDto(o));
+            }
+        }
+        return dtoList;
+    }
+
+    public List<OrderDTO> findOrdersByStatus(String status) {
+        List<Order> list = repo.findOrdersByStatus(status);
+        List<OrderDTO> dtoList = new ArrayList<>();
+        if (list != null && !list.isEmpty()) {
+            for (Order o: list) {
+                dtoList.add(OrderAdapter.toDto(o));
+            }
+        }
+        return dtoList;
+    }
 }
