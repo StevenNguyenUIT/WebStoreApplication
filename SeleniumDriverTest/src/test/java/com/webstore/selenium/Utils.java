e09ad627abdb96e9386469e42cf858720ef8dccf
vantypham <phamvanty102023@gmail.com>
2023-11-22 16:18:53 -0600

    Update Utils.java

--- SeleniumDriverTest/src/test/java/com/webstore/selenium/Utils.java
+++ SeleniumDriverTest/src/test/java/com/webstore/selenium/Utils.java
@@ -1,11 +1,11 @@
 package com.webstore.selenium;
 
 public class Utils {
-    private static String Windows_URL_Driver = "/Users/xhuyen/3_WAA/chromedive/chromedriver-mac-arm64/chromedriver";
-    private static String Windows_URL_Binary = "/Users/xhuyen/3_WAA/chromedive/chrome-headless-shell-mac-arm64/chrome-headless-shell";
+    private static String MacOS_URL_Driver = "/Users/xhuyen/3_WAA/chromedive/chromedriver-mac-arm64/chromedriver";
+    private static String MacOS_URL_Binary = "/Users/xhuyen/3_WAA/chromedive/chrome-headless-shell-mac-arm64/chrome-headless-shell";
 
-    private static String MacOS_URL_Driver = "C:\\tmp\\chromedriver-win64\\chromedriver.exe";
-    private static String MacOS_URL_Binary = "C:\\tmp\\chrome-headless-shell-win64\\chrome-headless-shell.exe";
+    private static String Windows_URL_Driver = "C:\\tmp\\chromedriver-win64\\chromedriver.exe";
+    private static String Windows_URL_Binary = "C:\\tmp\\chrome-headless-shell-win64\\chrome-headless-shell.exe";
 
     public static String getDriverURL() {
         String s = System.getProperty("os.name");
